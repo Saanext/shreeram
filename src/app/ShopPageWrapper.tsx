c7dc5a796a83087c3d749d43dcff1f7f7a82acef
Deepak Bagada <artechway@gmail.com>
2025-07-24 14:04:52 +0000

    useSearchParams() should be wrapped in a suspense boundary at page "/".

---
+++ src/app/ShopPageWrapper.tsx
@@ -0,0 +1,42 @@
+
+'use client'
+
+import * as React from 'react';
+import { Suspense } from 'react';
+import { ShopPageContent } from './ShopPageContent';
+import { Skeleton } from '@/components/ui/skeleton';
+
+function LoadingSkeleton() {
+  return (
+    <div className="container px-4 md:px-6 py-12 md:py-24 lg:py-32">
+       <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px] mb-12">
+          <div className="flex flex-col justify-center space-y-4">
+            <Skeleton className="h-12 w-3/4" />
+            <Skeleton className="h-6 w-full" />
+             <Skeleton className="h-6 w-2/3" />
+            <Skeleton className="h-12 w-40 mt-4" />
+          </div>
+          <Skeleton className="w-full aspect-square rounded-xl" />
+        </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-8">
+        {[...Array(8)].map((_, i) => (
+          <div key={i} className="flex flex-col gap-2">
+            <Skeleton className="aspect-square w-full" />
+            <Skeleton className="h-6 w-2/3" />
+            <Skeleton className="h-4 w-1/2" />
+            <Skeleton className="h-10 w-full mt-2" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+
+export default function ShopPageWrapper() {
+  return (
+    <Suspense fallback={<LoadingSkeleton />}>
+      <ShopPageContent />
+    </Suspense>
+  );
+}
